40e1f9aee0e4ad72b7166c8fcad18d037519cff2
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-04-02 18:00:18 +0700

    part 2

--- mergesort.cpp
+++ mergesort.cpp
@@ -26,4 +26,10 @@ void input()
     cout << "\n--------------------------" << endl;
     cout << "\nInputkan isi element array" << endl;
     cout << "\n--------------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Array index ke- " << i << " : ";
+        cin >> arr[i];
+    }
 }
